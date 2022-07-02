1606607bc8d48718e552029c1c63386f8b39526c
Y-v-a-i-n <107935034+Y-v-a-i-n@users.noreply.github.com>
2022-07-02 18:07:13 +0200

    Y-v-a-i-n/Passwd-Combinations

--- test/main.js
+++ test/main.js
@@ -2,7 +2,7 @@ const passwdCombinations=require("../index.js");
 var password="QKi8wXyD9TuKXfJL";
 var result=passwdCombinations.check(password);
 if(result<218340105584896){
-console.log("the password is very weak");
+    console.log("the password is very weak");
 }else if(result<4.767240170682353e+28){
     console.log("the password is weak");
 }else if(result<2.2726578844967515e+57){
